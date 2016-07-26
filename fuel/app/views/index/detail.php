18b13924af1b41e45f537ed3bbdf6a94af42a9b2
unknown <1160759@tad.asahi-np.co.jp>
2016-07-26 15:37:18 +0900

    delete meaningless files

--- fuel/app/views/index/detail.php
+++
@@ -1,8 +0,0 @@
-<ul class="nav nav-pills">
-	<li class='<?php echo Arr::get($subnav, "post" ); ?>'><?php echo Html::anchor('index/post','Post');?></li>
-	<li class='<?php echo Arr::get($subnav, "list" ); ?>'><?php echo Html::anchor('index/list','List');?></li>
-	<li class='<?php echo Arr::get($subnav, "search" ); ?>'><?php echo Html::anchor('index/search','Search');?></li>
-	<li class='<?php echo Arr::get($subnav, "detail" ); ?>'><?php echo Html::anchor('index/detail','Detail');?></li>
-
-</ul>
-<p>Detail</p>
